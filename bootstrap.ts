82816c1d994e5d16c0129df2f0ebc9321c38118e
Dotan Simha <dotansimha@gmail.com>
2016-01-24 17:40:09 +0200

    Initial commit

---
+++ bootstrap.ts
@@ -0,0 +1,22 @@
+import 'reflect-metadata';
+import {provide, Type, Provider, IterableDiffers} from 'angular2/core';
+import {MongoCursorDifferFactory} from 'angular2-meteor/mongo_cursor_differ';
+import {bootstrap as ng2Bootstrap} from 'angular2/bootstrap';
+
+function meteorProviders():Array {
+    let providers = [];
+
+    providers.push(provide(IterableDiffers, {
+        useValue: IterableDiffers.extend([new MongoCursorDifferFactory()])
+    }));
+
+    return providers;
+}
+
+export const METEOR_PROVIDERS = meteorProviders();
+
+export function bootstrap(appComponentType:any,
+                          providers:Array<Type | Provider | any[]> = null) {
+    //return ng2Bootstrap(appComponentType, [].concat(METEOR_PROVIDERS, providers || []));
+    ng2Bootstrap(appComponentType);
+}
