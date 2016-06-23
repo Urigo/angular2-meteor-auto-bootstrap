19404135516bfa38b87f823e86227fd3023b43bc
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-06-23 10:45:21 +0200

    feat: update to angular RC3 - RC2 at least to be working

--- modules/bootstrap.ts
+++ modules/bootstrap.ts
@@ -1,13 +1,13 @@
 import {Type, Provider, Component, ComponentRef} from '@angular/core';
-import {BROWSER_PROVIDERS} from '@angular/platform-browser/src/browser_common';
-import {BROWSER_APP_DYNAMIC_PROVIDERS} from '@angular/platform-browser-dynamic';
+import {BROWSER_PROVIDERS} from '@angular/platform-browser';
+import {BROWSER_APP_COMPILER_PROVIDERS} from '@angular/platform-browser-dynamic';
 import {MeteorApp as App, METEOR_PROVIDERS} from 'angular2-meteor';
 
 // Bootstrap with Meteor providers.
 export function bootstrap(component: any,
     providers: Array<Type | Provider | any[]> = null): Promise<ComponentRef<any>> {
   return App.bootstrap(component, BROWSER_PROVIDERS,
-    [BROWSER_APP_DYNAMIC_PROVIDERS, METEOR_PROVIDERS], providers);
+    [BROWSER_APP_COMPILER_PROVIDERS, METEOR_PROVIDERS], providers);
 }
 
 export function MeteorApp(args: any = {}) {
